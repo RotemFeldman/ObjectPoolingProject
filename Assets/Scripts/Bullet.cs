716426603852210ed303c19dcc9e9b11aac66c14
RotemFeldman <128305863+RotemFeldman@users.noreply.github.com>
2023-08-26 17:42:51 +0300

    complying with article 4 of homework
    gameobject.transform now => var transfrom.

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -8,16 +8,20 @@ public class Bullet : MonoBehaviour
     [SerializeField] float speed = 10f;
 
     float timeAlive;
-
+    
     const float setTimeAlive = 5f;
 
+    private void Start()
+    {
+        var tranfrom = gameObject.transform;
+    }
 
     void Update()
     {
         if (timeAlive > 0)
         {
             timeAlive -= Time.deltaTime;
-            gameObject.transform.Translate(0, 0, speed * Time.deltaTime);
+            transform.Translate(0, 0, speed * Time.deltaTime);
         }
         else
         {
